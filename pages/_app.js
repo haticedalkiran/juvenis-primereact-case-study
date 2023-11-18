c4ae1d0c2bcb8927679b5e3cdb3967ea48673179
Hatice Dalkıran <kocakkhatice@gmail.com>
2023-11-18 23:11:00 +0300

    install primereact

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
